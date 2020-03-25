3a1b8610d1e10cab8a5bb0c2becd1b9530cda107
Serguei Bezverkhi <sbezverk@cisco.com>
2020-03-25 08:23:46 -0400

    Adding marshalers for bgp-ls attributes
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/bgpls/bgpls-nlri.go
+++ pkg/bgpls/bgpls-nlri.go
@@ -1,6 +1,8 @@
 package bgpls
 
 import (
+	"encoding/json"
+
 	"github.com/golang/glog"
 	"github.com/sbezverk/gobmp/pkg/internal"
 )
@@ -8,18 +10,43 @@ import (
 // NLRI defines BGP-LS NLRI object as collection of BGP-LS TLVs
 // https://tools.ietf.org/html/rfc7752#section-3.3
 type NLRI struct {
-	LSTLV []TLV
+	LS []TLV
 }
 
 func (ls *NLRI) String() string {
 	var s string
 
 	s += "BGP-LS TLVs:" + "\n"
-	for _, tlv := range ls.LSTLV {
+	for _, tlv := range ls.LS {
 		s += tlv.String()
 	}
 
 	return s
+}
+
+// MarshalJSON defines a method to  BGP-LS TLV object into JSON format
+func (ls *NLRI) MarshalJSON() ([]byte, error) {
+	var jsonData []byte
+
+	jsonData = append(jsonData, '{')
+	jsonData = append(jsonData, []byte("\"BGPLSTLV\":")...)
+	jsonData = append(jsonData, '[')
+	if ls.LS != nil {
+		for i, tlv := range ls.LS {
+			b, err := json.Marshal(&tlv)
+			if err != nil {
+				return nil, err
+			}
+			jsonData = append(jsonData, b...)
+			if i < len(ls.LS)-1 {
+				jsonData = append(jsonData, ',')
+			}
+		}
+	}
+	jsonData = append(jsonData, ']')
+	jsonData = append(jsonData, '}')
+
+	return jsonData, nil
 }
 
 // UnmarshalBGPLSNLRI builds Prefix NLRI object
@@ -30,7 +57,7 @@ func UnmarshalBGPLSNLRI(b []byte) (*NLRI, error) {
 	if err != nil {
 		return nil, err
 	}
-	bgpls.LSTLV = ls
+	bgpls.LS = ls
 
 	return &bgpls, nil
 }
--- pkg/bgpls/bgpls-tlv.go
+++ pkg/bgpls/bgpls-tlv.go
@@ -2,6 +2,7 @@ package bgpls
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"net"
 
@@ -13,183 +14,295 @@ import (
 )
 
 // TLV defines BGP-LS TLV object
-// https://tools.ietf.org/html/rfc7752#section-3.3
+// https://tootlv.ietf.org/html/rfc7752#section-3.3
 type TLV struct {
 	Type   uint16
 	Length uint16
 	Value  []byte
 }
 
-func (ls *TLV) String() string {
+func (tlv *TLV) String() string {
 	var s string
-	switch ls.Type {
-
-	// List of TLV to skip processing
-
-	case 1173:
-		break
-
-	// List of TLV requireing additional processing
-
+	switch tlv.Type {
 	case 258:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Link Local/Remote Identifiers)\n", ls.Type)
-		lid := binary.BigEndian.Uint32(ls.Value)
-		rid := binary.BigEndian.Uint32(ls.Value[4:])
-		s += fmt.Sprintf("      Link Local: %d\n", lid)
-		s += fmt.Sprintf("      Link Remote: %d\n", rid)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Link Local/Remote Identifiers)\n", tlv.Type)
+		lri, err := base.UnmarshalLocalRemoteIdentifierTLV(tlv.Value)
+		if err != nil {
+			s += err.Error() + "\n"
+			break
+		}
+		s += lri.String()
 	case 263:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Multi-Topology Identifier)\n", ls.Type)
-		mit, err := base.UnmarshalMultiTopologyIdentifierTLV(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Multi-Topology Identifier)\n", tlv.Type)
+		mit, err := base.UnmarshalMultiTopologyIdentifierTLV(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += mit.String()
 	case 266:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Node MSD)\n", ls.Type)
-		msd, err := base.UnmarshalNodeMSD(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Node MSD)\n", tlv.Type)
+		msd, err := base.UnmarshalNodeMSD(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += msd.String()
 	case 267:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Link MSD)\n", ls.Type)
-		msd, err := base.UnmarshalLinkMSD(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Link MSD)\n", tlv.Type)
+		msd, err := base.UnmarshalLinkMSD(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += msd.String()
 	case 1026:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Node Name)\n", ls.Type)
-		s += fmt.Sprintf("      Node Name: %s\n", string(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Node Name)\n", tlv.Type)
+		s += fmt.Sprintf("      Node Name: %s\n", string(tlv.Value))
 	case 1027:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IS-IS Area Identifier)\n", ls.Type)
-		s += fmt.Sprintf("      IS-IS Area Identifier: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IS-IS Area Identifier)\n", tlv.Type)
+		s += fmt.Sprintf("      IS-IS Area Identifier: %s\n", internal.MessageHex(tlv.Value))
 	case 1028:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv4 Router-ID of Local Node)\n", ls.Type)
-		s += fmt.Sprintf("      IPv4 Router-ID of Local Node: %s\n", net.IP(ls.Value).To4().String())
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv4 Router-ID of Local Node)\n", tlv.Type)
+		s += fmt.Sprintf("      IPv4 Router-ID of Local Node: %s\n", net.IP(tlv.Value).To4().String())
 	case 1029:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv6 Router-ID of Local Node)\n", ls.Type)
-		s += fmt.Sprintf("      IPv6 Router-ID of Local Node: %s\n", net.IP(ls.Value).To16().String())
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv6 Router-ID of Local Node)\n", tlv.Type)
+		s += fmt.Sprintf("      IPv6 Router-ID of Local Node: %s\n", net.IP(tlv.Value).To16().String())
 	case 1030:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv4 Router-ID of Remote Node)\n", ls.Type)
-		s += fmt.Sprintf("      IPv4 Router-ID of Remote Node: %s\n", net.IP(ls.Value).To4().String())
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv4 Router-ID of Remote Node)\n", tlv.Type)
+		s += fmt.Sprintf("      IPv4 Router-ID of Remote Node: %s\n", net.IP(tlv.Value).To4().String())
 	case 1031:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv6 Router-ID of Remote Node)\n", ls.Type)
-		s += fmt.Sprintf("      IPv6 Router-ID of Remote Node: %s\n", net.IP(ls.Value).To16().String())
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IPv6 Router-ID of Remote Node)\n", tlv.Type)
+		s += fmt.Sprintf("      IPv6 Router-ID of Remote Node: %s\n", net.IP(tlv.Value).To16().String())
 	case 1034:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SR Capabilities)\n", ls.Type)
-		s += fmt.Sprintf("      SR Capabilities: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SR Capabilities)\n", tlv.Type)
+		s += fmt.Sprintf("      SR Capabilities: %s\n", internal.MessageHex(tlv.Value))
 	case 1035:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SR Algorithm)\n", ls.Type)
-		s += fmt.Sprintf("      SR Algorithm: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SR Algorithm)\n", tlv.Type)
+		s += fmt.Sprintf("      SR Algorithm: %s\n", internal.MessageHex(tlv.Value))
 	case 1036:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SR Local Block)\n", ls.Type)
-		s += fmt.Sprintf("      SR Local Block: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SR Local Block)\n", tlv.Type)
+		s += fmt.Sprintf("      SR Local Block: %s\n", internal.MessageHex(tlv.Value))
 	case 1038:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Capabilities TLV)\n", ls.Type)
-		cap, err := srv6.UnmarshalSRv6CapabilityTLV(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Capabilities TLV)\n", tlv.Type)
+		cap, err := srv6.UnmarshalSRv6CapabilityTLV(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += cap.String(2)
 	case 1088:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Administrative group (color))\n", ls.Type)
-		s += fmt.Sprintf("      Administrative group (color): %d\n", binary.BigEndian.Uint32(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Administrative group (color))\n", tlv.Type)
+		s += fmt.Sprintf("      Administrative group (color): %d\n", binary.BigEndian.Uint32(tlv.Value))
 	case 1089:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Maximum link bandwidth)\n", ls.Type)
-		s += fmt.Sprintf("      Maximum link bandwidth: %d\n", binary.BigEndian.Uint32(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Maximum link bandwidth)\n", tlv.Type)
+		s += fmt.Sprintf("      Maximum link bandwidth: %d\n", binary.BigEndian.Uint32(tlv.Value))
 	case 1090:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Max. reservable link bandwidth)\n", ls.Type)
-		s += fmt.Sprintf("      Max. reservable link bandwidth: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Max. reservable link bandwidth)\n", tlv.Type)
+		s += fmt.Sprintf("      Max. reservable link bandwidth: %s\n", internal.MessageHex(tlv.Value))
 	case 1091:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Unreserved bandwidth)\n", ls.Type)
-		s += fmt.Sprintf("      Unreserved bandwidth: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Unreserved bandwidth)\n", tlv.Type)
+		s += fmt.Sprintf("      Unreserved bandwidth: %s\n", internal.MessageHex(tlv.Value))
 	case 1092:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IGP Metric)\n", ls.Type)
-		m := binary.BigEndian.Uint32(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (IGP Metric)\n", tlv.Type)
+		m := binary.BigEndian.Uint32(tlv.Value)
 		s += fmt.Sprintf("      IGP Metric: %d\n", m)
 	case 1095:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (TE Default Metric)\n", ls.Type)
-		s += fmt.Sprintf("      TE Default Metric: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (TE Default Metric)\n", tlv.Type)
+		s += fmt.Sprintf("      TE Default Metric: %s\n", internal.MessageHex(tlv.Value))
 	case 1099:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Adjacency Segment Identifier)\n", ls.Type)
-		asid, err := sr.UnmarshalAdjacencySIDTLV(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Adjacency Segment Identifier)\n", tlv.Type)
+		asid, err := sr.UnmarshalAdjacencySIDTLV(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += asid.String()
 	case 1106:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 End.X SID TLV)\n", ls.Type)
-		endx, err := srv6.UnmarshalSRv6EndXSIDTLV(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 End.X SID TLV)\n", tlv.Type)
+		endx, err := srv6.UnmarshalSRv6EndXSIDTLV(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += endx.String(1)
 	case 1155:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Prefix Metric)\n", ls.Type)
-		m := binary.BigEndian.Uint32(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Prefix Metric)\n", tlv.Type)
+		m := binary.BigEndian.Uint32(tlv.Value)
 		s += fmt.Sprintf("      Prefix Metric: %d\n", m)
 	case 1158:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Prefix SID)\n", ls.Type)
-		psid, err := sr.UnmarshalPrefixSIDTLV(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Prefix SID)\n", tlv.Type)
+		psid, err := sr.UnmarshalPrefixSIDTLV(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += psid.String()
 	case 1162:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Locator TLV)\n", ls.Type)
-		l, err := srv6.UnmarshalSRv6LocatorTLV(ls.Value)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Locator TLV)\n", tlv.Type)
+		l, err := srv6.UnmarshalSRv6LocatorTLV(tlv.Value)
 		if err != nil {
 			s += err.Error() + "\n"
 			break
 		}
 		s += l.String(1)
 	case 1170:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Prefix Attributes Flags)\n", ls.Type)
-		s += fmt.Sprintf("      Flag: %s\n", internal.MessageHex(ls.Value))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Prefix Attributes Flags)\n", tlv.Type)
+		s += fmt.Sprintf("      Flag: %s\n", internal.MessageHex(tlv.Value))
 	case 1171:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Source Router-ID)\n", ls.Type)
-		if ls.Length == 4 {
-			s += fmt.Sprintf("   Source Router-ID: %s\n", net.IP(ls.Value).To4().String())
-		} else if ls.Length == 16 {
-			s += fmt.Sprintf("   Source Router-ID: %s\n", net.IP(ls.Value).To16().String())
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Source Router-ID)\n", tlv.Type)
+		if tlv.Length == 4 {
+			s += fmt.Sprintf("   Source Router-ID: %s\n", net.IP(tlv.Value).To4().String())
+		} else if tlv.Length == 16 {
+			s += fmt.Sprintf("   Source Router-ID: %s\n", net.IP(tlv.Value).To16().String())
 		}
+	case 1173:
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (Extended Administrative Group)\n", tlv.Type)
+		s += fmt.Sprintf("      Color: %s\n", internal.MessageHex(tlv.Value))
 	case 1250:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Endpoint Function)\n", ls.Type)
-		s += fmt.Sprintf("      Endpoint Behavior: %s\n", internal.MessageHex(ls.Value[:2]))
-		s += fmt.Sprintf("      Flag: %02x\n", ls.Value[2])
-		s += fmt.Sprintf("      Algorithm: %d\n", ls.Value[3])
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Endpoint Function)\n", tlv.Type)
+		s += fmt.Sprintf("      Endpoint Behavior: %s\n", internal.MessageHex(tlv.Value[:2]))
+		s += fmt.Sprintf("      Flag: %02x\n", tlv.Value[2])
+		s += fmt.Sprintf("      Algorithm: %d\n", tlv.Value[3])
 	case 1251:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Endpoint Function)\n", ls.Type)
-		s += fmt.Sprintf("      Flag: %02x\n", ls.Value[0])
-		s += fmt.Sprintf("      Weight: %d\n", ls.Value[1])
-		s += fmt.Sprintf("      Peer AS Number: %d\n", binary.BigEndian.Uint32(ls.Value[4:8]))
-		s += fmt.Sprintf("      Peer BGP Identifier: %s\n", internal.MessageHex(ls.Value[8:12]))
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 Endpoint Function)\n", tlv.Type)
+		s += fmt.Sprintf("      Flag: %02x\n", tlv.Value[0])
+		s += fmt.Sprintf("      Weight: %d\n", tlv.Value[1])
+		s += fmt.Sprintf("      Peer AS Number: %d\n", binary.BigEndian.Uint32(tlv.Value[4:8]))
+		s += fmt.Sprintf("      Peer BGP Identifier: %s\n", internal.MessageHex(tlv.Value[8:12]))
 	case 1252:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 SID Structure)\n", ls.Type)
-		s += fmt.Sprintf("      LB Length: %d\n", ls.Value[0])
-		s += fmt.Sprintf("      LN Length: %d\n", ls.Value[1])
-		s += fmt.Sprintf("      Function Length: %d\n", ls.Value[2])
-		s += fmt.Sprintf("      Argument Length: %d\n", ls.Value[3])
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d (SRv6 SID Structure)\n", tlv.Type)
+		s += fmt.Sprintf("      LB Length: %d\n", tlv.Value[0])
+		s += fmt.Sprintf("      LN Length: %d\n", tlv.Value[1])
+		s += fmt.Sprintf("      Function Length: %d\n", tlv.Value[2])
+		s += fmt.Sprintf("      Argument Length: %d\n", tlv.Value[3])
 
 	// Default BGP-LS TLV processing
 
 	default:
-		s += fmt.Sprintf("   BGP-LS TLV Type: %d\n", ls.Type)
-		s += fmt.Sprintf("   BGP-LS TLV Length: %d\n", ls.Length)
+		s += fmt.Sprintf("   BGP-LS TLV Type: %d\n", tlv.Type)
+		s += fmt.Sprintf("   BGP-LS TLV Length: %d\n", tlv.Length)
 		s += "      Value: "
-		s += internal.MessageHex(ls.Value)
+		s += internal.MessageHex(tlv.Value)
 		s += "\n"
 	}
 
 	return s
+}
+
+// func (tlv *LinkDescriptorTLV) String() string {
+// 	var s string
+// 	switch tlv.Type {
+// 	case 258:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d (Link Local/Remote Identifiers)\n", tlv.Type)
+// 		lri, err := UnmarshalLocalRemoteIdentifierTLV(tlv.Value)
+// 		if err != nil {
+// 			s += err.Error() + "\n"
+// 			break
+// 		}
+// 		s += lri.String()
+// 	case 259:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d (IPv4 interface address)\n", tlv.Type)
+// 		s += fmt.Sprintf("      IPv4 interface address: %s\n", net.IP(tlv.Value).To4().String())
+// 	case 260:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d (IPv4 neighbor address)\n", tlv.Type)
+// 		s += fmt.Sprintf("      IPv4 neighbor address: %s\n", net.IP(tlv.Value).To4().String())
+// 	case 261:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d (IPv6 interface address)\n", tlv.Type)
+// 		s += fmt.Sprintf("      IPv6 interface address: %s\n", net.IP(tlv.Value).To16().String())
+// 	case 262:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d (IPv6 neighbor address)\n", tlv.Type)
+// 		s += fmt.Sprintf("      IPv6 neighbor address: %s\n", net.IP(tlv.Value).To16().String())
+// 	case 263:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d (Multi-Topology Identifier)\n", tlv.Type)
+// 		mit, err := UnmarshalMultiTopologyIdentifierTLV(tlv.Value)
+// 		if err != nil {
+// 			s += err.Error() + "\n"
+// 			break
+// 		}
+// 		s += mit.String()
+// 	default:
+// 		s += fmt.Sprintf("   Link Descriptor TLV Type: %d\n", tlv.Type)
+// 		s += fmt.Sprintf("   Link Descriptor TLV Length: %d\n", tlv.Length)
+// 		s += "      Value: "
+// 		s += internal.MessageHex(tlv.Value)
+// 		s += "\n"
+// 	}
+
+// 	return s
+// }
+
+// case 259:
+// 	jsonData = append(jsonData, []byte(fmt.Sprintf("\"IPv4 interface address\","))...)
+// 	jsonData = append(jsonData, []byte("\"ipv4InterfaceAddress\":")...)
+// 	jsonData = append(jsonData, internal.RawBytesToJSON(ltlv.Value)...)
+// case 260:
+// 	jsonData = append(jsonData, []byte(fmt.Sprintf("\"IPv4 neighbor address\","))...)
+// 	jsonData = append(jsonData, []byte("\"ipv4NeighborAddress\":")...)
+// 	jsonData = append(jsonData, internal.RawBytesToJSON(ltlv.Value)...)
+// case 261:
+// 	jsonData = append(jsonData, []byte(fmt.Sprintf("\"IPv6 interface address\","))...)
+// 	jsonData = append(jsonData, []byte("\"ipv6InterfaceAddress\":")...)
+// 	jsonData = append(jsonData, internal.RawBytesToJSON(ltlv.Value)...)
+// case 262:
+// 	jsonData = append(jsonData, []byte(fmt.Sprintf("\"IPv6 neighbor address\","))...)
+// 	jsonData = append(jsonData, []byte("\"ipv6NeighborAddress\":")...)
+// 	jsonData = append(jsonData, internal.RawBytesToJSON(ltlv.Value)...)
+
+// MarshalJSON defines a method to Marshal BGP-LS TLV object into JSON format
+func (tlv *TLV) MarshalJSON() ([]byte, error) {
+	var jsonData []byte
+	var b []byte
+
+	jsonData = append(jsonData, '{')
+	jsonData = append(jsonData, []byte("\"Type\":")...)
+	jsonData = append(jsonData, []byte(fmt.Sprintf("%d,", tlv.Type))...)
+	jsonData = append(jsonData, []byte("\"Description\":")...)
+	switch tlv.Type {
+	case 258:
+		jsonData = append(jsonData, []byte(fmt.Sprintf("\"Local/Remote Identifiers\","))...)
+		jsonData = append(jsonData, []byte("\"identifiersLocalRemote\":")...)
+		lri, err := base.UnmarshalLocalRemoteIdentifierTLV(tlv.Value)
+		if err != nil {
+			return nil, err
+		}
+		b, err = json.Marshal(lri)
+		if err != nil {
+			return nil, err
+		}
+		jsonData = append(jsonData, b...)
+	case 263:
+		jsonData = append(jsonData, []byte(fmt.Sprintf("\"Multi-Topology Identifier\","))...)
+		jsonData = append(jsonData, []byte("\"multiTopologyIdentifier\":")...)
+		mit, err := base.UnmarshalMultiTopologyIdentifierTLV(tlv.Value)
+		if err != nil {
+			return nil, err
+		}
+		b, err = json.Marshal(mit)
+		if err != nil {
+			return nil, err
+		}
+		jsonData = append(jsonData, b...)
+	case 266:
+		jsonData = append(jsonData, []byte(fmt.Sprintf("\"Node MSD\","))...)
+		jsonData = append(jsonData, []byte("\"nodeMSD\":")...)
+		msd, err := base.UnmarshalNodeMSD(tlv.Value)
+		if err != nil {
+			return nil, err
+		}
+		b, err = json.Marshal(msd)
+		if err != nil {
+			return nil, err
+		}
+		jsonData = append(jsonData, b...)
+	default:
+		jsonData = append(jsonData, []byte(fmt.Sprintf("\"Unknown BGP-LS TLV\","))...)
+		jsonData = append(jsonData, []byte("\"Value\":")...)
+		jsonData = append(jsonData, internal.RawBytesToJSON(tlv.Value)...)
+	}
+	jsonData = append(jsonData, '}')
+
+	return jsonData, nil
 }
 
 // UnmarshalBGPLSTLV builds Collection of BGP-LS TLVs
