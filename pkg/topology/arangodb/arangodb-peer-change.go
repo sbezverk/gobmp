b3b8d93751952e282c698e77b8e584f81ec0f8a7
brmcdoug <brmcdoug@cisco.com>
2020-04-06 09:36:29 -0700

    PeerChange

---
+++ pkg/topology/arangodb/arangodb-peer-change.go
@@ -0,0 +1,19 @@
+package arangodb
+
+func (m *arangoDB) peerChangeHandler(obj *message.PeerStateChange) {
+	glog.Infof("><SB> peer change handler - update db with peer change message data")
+
+	bgp_peer_document := &database.BGPPeer{
+		RouterID:	message.LocalBGPID
+		LocalIP:	message.LocalIP
+		LocalASN:	message.LocalASN
+		PeerIP:		message.RemoteIP
+		PeerBGPID:	message.RemoteBGPID
+		PeerASN:	message.RemoteASN
+	}
+	if err := m.arangoDB.Upsert(bgp_peer_document); err != nil {
+		fmt.Println("Encountered an error while upserting the bgp peer document", err)
+	} else {
+		fmt.Printf("Successfully added bgp peer document: %q with peer: %q\n", LocalBGPID, RemoteIP)
+	}
+}
