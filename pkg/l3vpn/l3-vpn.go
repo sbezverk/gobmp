a1420c427374034f49ad83607b0fd28494e0e73d
Serguei Bezverkhi <sbezverk@cisco.com>
2020-04-07 08:51:11 -0400

    Add label processing func and unit test
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/l3vpn/l3-vpn.go
+++ pkg/l3vpn/l3-vpn.go
@@ -1,15 +1,31 @@
 package l3vpn
 
+import (
+	"encoding/binary"
+	"fmt"
+)
+
 // Label defines a structure of a single label
 type Label struct {
 	Value uint32
 	Exp   uint8 // 3 bits
 	BoS   bool  // 1 bit
-	TTL   uint8 // 8 bits
 }
 
-func NewLabel(b []byte) Label {
-	return Label{}
+func makeLabel(b []byte) (*Label, error) {
+	l := Label{}
+	if len(b) != 3 {
+		return nil, fmt.Errorf("invalid length of slice, expected 3 got %d", len(b))
+	}
+	v := make([]byte, 4)
+	copy(v[1:], b)
+	l.Value = binary.BigEndian.Uint32(v)
+	l.Value = l.Value >> 4
+	// Move Exp bits to the beggining of the byte and leave only 3 bits, mask the rest.
+	l.Exp = uint8(b[2]&0x07) >> 1
+	l.BoS = b[2]&0x01 == 1
+
+	return &l, nil
 }
 
 // RD defines a structure of VPN prefixe's Route Distinguisher
@@ -28,7 +44,7 @@ func (rd *RD) GetRD() string {
 // NLRI defines L3 VPN NLRI object
 type NLRI struct {
 	Length uint8
-	Labels []Label
+	Labels []*Label
 	RD     RD
 	Prefix []byte
 }
@@ -40,13 +56,16 @@ func UnmarshalL3VPNNLRI(b []byte) (*NLRI, error) {
 	// Getting length of NLRI in bytes
 	n.Length = uint8(b[p] / 8)
 	p++
-	n.Labels = make([]Label, 0)
+	n.Labels = make([]*Label, 0)
 	// subtract 12 for the length as label stack follows by RD 8 bytes and prefix 4 bytes
 	for p < len(b)-12 {
-		l := NewLabel(b[p : p+3])
+		l, err := makeLabel(b[p : p+3])
+		if err != nil {
+			return nil, err
+		}
 		n.Labels = append(n.Labels, l)
 		p += 3
 	}
 
-	return nil, nil
+	return &n, nil
 }
--- pkg/l3vpn/l3vpn_test.go
+++ pkg/l3vpn/l3vpn_test.go
@@ -10,17 +10,45 @@ func TestMakeLabel(t *testing.T) {
 		name   string
 		input  []byte
 		expect *Label
+		fail   bool
 	}{
 		{
-			name:   "label 1",
-			input:  []byte{},
-			expect: &Label{},
+			name:  "label 1",
+			input: []byte{5, 220, 33},
+			expect: &Label{
+				Value: 24002,
+				Exp:   0,
+				BoS:   true,
+			},
+			fail: false,
+		},
+		{
+			name:  "label 2",
+			input: []byte{5, 220, 65},
+			expect: &Label{
+				Value: 24004,
+				Exp:   0,
+				BoS:   true,
+			},
+			fail: false,
+		},
+		{
+			name:   "wrong length",
+			input:  []byte{5, 220, 33, 0},
+			expect: nil,
+			fail:   true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := makeLabel(tt.input)
-			if !reflect.DeepEqual(&got, tt.expect) {
+			got, err := makeLabel(tt.input)
+			if err != nil && !tt.fail {
+				t.Fatalf("expected to succeed but failed with error: %+v", err)
+			}
+			if err == nil && tt.fail {
+				t.Fatalf("expected to fail but succeeded")
+			}
+			if !reflect.DeepEqual(got, tt.expect) {
 				t.Errorf("Expected label %+v does not match to actual label %+v", got, *tt.expect)
 			}
 		})
