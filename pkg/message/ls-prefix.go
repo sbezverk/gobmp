73b3c3397c5d9b48168e3ef03ff4d20feda2c7c4
Serguei Bezverkhi <sbezverk@cisco.com>
2020-04-08 13:15:54 -0400

    initial lsprefix definition
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

---
+++ pkg/message/ls-prefix.go
@@ -0,0 +1,69 @@
+package message
+
+import (
+	"net"
+
+	"github.com/sbezverk/gobmp/pkg/bgp"
+	"github.com/sbezverk/gobmp/pkg/bmp"
+)
+
+func (p *producer) lsPrefix(operation string, ph *bmp.PerPeerHeader, update *bgp.Update) (*LSPrefix, error) {
+	nlri14, err := update.GetNLRI14()
+	if err != nil {
+		return nil, err
+	}
+	nlri71, err := nlri14.GetNLRI71()
+	if err != nil {
+		return nil, err
+	}
+	msg := LSPrefix{
+		Action:       operation,
+		RouterHash:   p.speakerHash,
+		RouterIP:     p.speakerIP,
+		BaseAttrHash: update.GetBaseAttrHash(),
+		PeerHash:     ph.GetPeerHash(),
+		PeerASN:      ph.PeerAS,
+		Timestamp:    ph.PeerTimestamp,
+	}
+	msg.Nexthop = nlri14.GetNextHop()
+	if ph.FlagV {
+		// IPv6 specific conversions
+		msg.PeerIP = net.IP(ph.PeerAddress).To16().String()
+	} else {
+		// IPv4 specific conversions
+		msg.PeerIP = net.IP(ph.PeerAddress[12:]).To4().String()
+	}
+	// Processing other nlri and attributes, since they are optional, processing only if they exist
+	nlri71.GetLinkNLRI()
+	prefix, err := nlri71.GetPrefixNLRI()
+	if err == nil {
+		msg.Protocol = prefix.GetLinkProtocolID()
+		msg.LSID = prefix.GetLinkLSID(true)
+		msg.OSPFAreaID = prefix.GetLinkOSPFAreaID(true)
+		if ph.FlagV {
+		} else {
+		}
+		msg.LocalNodeHash = prefix.LocalNodeHash
+		msg.IGPRouterID = prefix.GetLocalIGPRouterID()
+	}
+	lsprefix, err := update.GetNLRI29()
+	if err == nil {
+		if ph.FlagV {
+			msg.RouterID = lsprefix.GetLocalIPv6RouterID()
+		} else {
+			msg.RouterID = lsprefix.GetLocalIPv4RouterID()
+		}
+		msg.MTID = lsprefix.GetMTID()
+		msg.ISISAreaID = lsprefix.GetISISAreaID()
+		msg.IGPMetric = lsprefix.GetIGPMetric()
+	}
+	msg.ASPath = update.GetAttrASPath(p.as4Capable)
+	if med := update.GetAttrMED(); med != nil {
+		msg.MED = *med
+	}
+	if lp := update.GetAttrLocalPref(); lp != nil {
+		msg.LocalPref = *lp
+	}
+
+	return &msg, nil
+}
