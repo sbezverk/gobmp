7fc97cc8793ccb06c3c9b26f23a2c69e50db68d6
Serguei Bezverkhi <sbezverk@cisco.com>
2020-04-22 16:55:32 -0400

    fine tune json names
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/sr/sr-sid-tlv.go
+++ pkg/sr/sr-sid-tlv.go
@@ -2,7 +2,7 @@ package sr
 
 // SIDTLV defines SID Sub tlv object
 type SIDTLV struct {
-	Type   uint16
-	Length uint16
+	Type   uint16 `json:"-"`
+	Length uint16 `json:"-"`
 	Value  []byte `json:"sid,omitempty"`
 }
