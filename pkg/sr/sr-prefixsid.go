c8e889a10a1e0801690ad83b15794cf41291ec50
Serguei Bezverkhi <sbezverk@cisco.com>
2020-05-05 12:48:09 -0400

    fine tune json tags for flags
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/sr/sr-prefixsid.go
+++ pkg/sr/sr-prefixsid.go
@@ -5,20 +5,41 @@ import (
 	"github.com/sbezverk/gobmp/pkg/tools"
 )
 
+// Flags carries PrefixSID flag bits
+//  0 1 2 3 4 5 6 7
+// +-+-+-+-+-+-+-+-+
+// |R|N|P|E|V|L|   |
+// +-+-+-+-+-+-+-+-+
+type Flags struct {
+	R bool `json:"r_flag"`
+	N bool `json:"n_flag"`
+	P bool `json:"p_flag"`
+	E bool `json:"e_flag"`
+	V bool `json:"v_flag"`
+	L bool `json:"l_flag"`
+}
+
 // PrefixSIDTLV defines Prefix SID TLV Object
 // https://tools.ietf.org/html/draft-ietf-idr-bgp-ls-segment-routing-ext-08#section-2.3.1
 type PrefixSIDTLV struct {
-	Flags     uint8
-	Algorithm uint8
-	SID       []byte
+	Flags     *Flags `json:"flags,omitempty"`
+	Algorithm uint8  `json:"algo,omitempty"`
+	SID       []byte `json:"sid,omitempty"`
 }
 
 // UnmarshalPrefixSIDTLV builds Prefix SID TLV Object
 func UnmarshalPrefixSIDTLV(b []byte) (*PrefixSIDTLV, error) {
 	glog.V(6).Infof("Prefix SID TLV Raw: %s", tools.MessageHex(b))
-	psid := PrefixSIDTLV{}
+	psid := PrefixSIDTLV{
+		Flags: &Flags{},
+	}
 	p := 0
-	psid.Flags = b[p]
+	psid.Flags.R = b[p]&128 == 1
+	psid.Flags.N = b[p]&64 == 1
+	psid.Flags.P = b[p]&32 == 1
+	psid.Flags.E = b[p]&16 == 1
+	psid.Flags.V = b[p]&8 == 1
+	psid.Flags.L = b[p]&4 == 1
 	p++
 	psid.Algorithm = b[p]
 	p++
