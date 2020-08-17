929c1b259f87c58878ced0c039e7966706dc2e3b
Serguei Bezverkhi <sbezverk@cisco.com>
2020-08-17 09:47:03 -0400

    fix panic for route refresh message
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/bmp/route-monitor.go
+++ pkg/bmp/route-monitor.go
@@ -1,7 +1,7 @@
 package bmp
 
 import (
-	"encoding/binary"
+	"fmt"
 
 	"github.com/golang/glog"
 	"github.com/sbezverk/gobmp/pkg/bgp"
@@ -17,16 +17,27 @@ type RouteMonitor struct {
 func UnmarshalBMPRouteMonitorMessage(b []byte) (*RouteMonitor, error) {
 	glog.V(6).Infof("BMP Route Monitor Message Raw: %s", tools.MessageHex(b))
 	rm := RouteMonitor{}
+	if len(b) < 19 {
+		return nil, fmt.Errorf("malformed route monitor message")
+	}
 	p := 0
 	// Skip 16 bytes of a marker
 	p += 16
-	l := binary.BigEndian.Uint16(b[p : p+2])
+	// Skip 2 bytes of the update length
 	p += 2
-	u, err := bgp.UnmarshalBGPUpdate(b[p+1 : p+int(l-18)])
-	if err != nil {
-		return nil, err
+	// Getting update type, currently only type 2 is processed
+	t := b[p]
+	p++
+	switch t {
+	case 2:
+		// UPdate type
+		u, err := bgp.UnmarshalBGPUpdate(b[p:])
+		if err != nil {
+			return nil, err
+		}
+		rm.Update = u
+	default:
 	}
-	rm.Update = u
 
 	return &rm, nil
 }
