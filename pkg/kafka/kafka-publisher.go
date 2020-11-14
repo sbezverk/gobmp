72af63dfc7b5042e860454bd316d61b332e084af
Serguei Bezverkhi <sbezverk@cisco.com>
2020-11-14 12:05:16 -0500

    Add publishing bits for SR Policy
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/kafka/kafka-publisher.go
+++ pkg/kafka/kafka-publisher.go
@@ -16,18 +16,21 @@ import (
 
 // Define constants for each topic name
 const (
-	peerTopic             = "gobmp.parsed.peer"
-	unicastMessageTopic   = "gobmp.parsed.unicast_prefix"
-	unicastMessageV4Topic = "gobmp.parsed.unicast_prefix_v4"
-	unicastMessageV6Topic = "gobmp.parsed.unicast_prefix_v6"
-	lsNodeMessageTopic    = "gobmp.parsed.ls_node"
-	lsLinkMessageTopic    = "gobmp.parsed.ls_link"
-	l3vpnMessageTopic     = "gobmp.parsed.l3vpn"
-	l3vpnMessageV4Topic   = "gobmp.parsed.l3vpn_v4"
-	l3vpnMessageV6Topic   = "gobmp.parsed.l3vpn_v6"
-	lsPrefixMessageTopic  = "gobmp.parsed.ls_prefix"
-	lsSRv6SIDMessageTopic = "gobmp.parsed.ls_srv6_sid"
-	evpnMessageTopic      = "gobmp.parsed.evpn"
+	peerTopic              = "gobmp.parsed.peer"
+	unicastMessageTopic    = "gobmp.parsed.unicast_prefix"
+	unicastMessageV4Topic  = "gobmp.parsed.unicast_prefix_v4"
+	unicastMessageV6Topic  = "gobmp.parsed.unicast_prefix_v6"
+	lsNodeMessageTopic     = "gobmp.parsed.ls_node"
+	lsLinkMessageTopic     = "gobmp.parsed.ls_link"
+	l3vpnMessageTopic      = "gobmp.parsed.l3vpn"
+	l3vpnMessageV4Topic    = "gobmp.parsed.l3vpn_v4"
+	l3vpnMessageV6Topic    = "gobmp.parsed.l3vpn_v6"
+	lsPrefixMessageTopic   = "gobmp.parsed.ls_prefix"
+	lsSRv6SIDMessageTopic  = "gobmp.parsed.ls_srv6_sid"
+	evpnMessageTopic       = "gobmp.parsed.evpn"
+	srPolicyMessageTopic   = "gobmp.parsed.sr_policy"
+	srPolicyMessageV4Topic = "gobmp.parsed.sr_policy_v4"
+	srPolicyMessageV6Topic = "gobmp.parsed.sr_policy_v6"
 )
 
 var (
@@ -53,6 +56,9 @@ var (
 		lsPrefixMessageTopic,
 		lsSRv6SIDMessageTopic,
 		evpnMessageTopic,
+		srPolicyMessageTopic,
+		srPolicyMessageV4Topic,
+		srPolicyMessageV6Topic,
 	}
 )
 
@@ -89,6 +95,12 @@ func (p *publisher) PublishMessage(t int, key []byte, msg []byte) error {
 		return p.produceMessage(lsSRv6SIDMessageTopic, key, msg)
 	case bmp.EVPNMsg:
 		return p.produceMessage(evpnMessageTopic, key, msg)
+	case bmp.SRPolicyMsg:
+		return p.produceMessage(srPolicyMessageTopic, key, msg)
+	case bmp.SRPolicyV4Msg:
+		return p.produceMessage(srPolicyMessageV4Topic, key, msg)
+	case bmp.SRPolicyV6Msg:
+		return p.produceMessage(srPolicyMessageV6Topic, key, msg)
 	}
 
 	return fmt.Errorf("not implemented")
--- pkg/message/types.go
+++ pkg/message/types.go
@@ -316,3 +316,32 @@ type EVPNPrefix struct {
 	// https://tools.ietf.org/html/rfc6514
 	// Add to the message
 }
+
+// SRPolicy defines the structure of SR Policy message
+type SRPolicy struct {
+	Key            string              `json:"_key,omitempty"`
+	ID             string              `json:"_id,omitempty"`
+	Rev            string              `json:"_rev,omitempty"`
+	Action         string              `json:"action,omitempty"` // Action can be "add" or "del"
+	Sequence       int                 `json:"sequence,omitempty"`
+	Hash           string              `json:"hash,omitempty"`
+	RouterHash     string              `json:"router_hash,omitempty"`
+	RouterIP       string              `json:"router_ip,omitempty"`
+	BaseAttributes *bgp.BaseAttributes `json:"base_attrs,omitempty"`
+	PeerHash       string              `json:"peer_hash,omitempty"`
+	PeerIP         string              `json:"peer_ip,omitempty"`
+	PeerASN        int32               `json:"peer_asn,omitempty"`
+	Timestamp      string              `json:"timestamp,omitempty"`
+	IsIPv4         bool                `json:"is_ipv4"`
+	OriginAS       int32               `json:"origin_as,omitempty"`
+	Nexthop        string              `json:"nexthop,omitempty"`
+	ClusterList    string              `json:"cluster_list,omitempty"`
+	IsNexthopIPv4  bool                `json:"is_nexthop_ipv4"`
+	PathID         int32               `json:"path_id,omitempty"`
+	Labels         []uint32            `json:"labels,omitempty"`
+	IsPrepolicy    bool                `json:"is_prepolicy"`
+	IsAdjRIBIn     bool                `json:"is_adj_rib_in"`
+	Distinguisher  uint32              `json:"distinguisher,omitempty"`
+	Color          uint32              `json:"color,omitempty"`
+	Endpoint       []byte              `json:"endpoint,omitempty"`
+}
