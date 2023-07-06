c82446468718a677a66d022999b15dea59e39ee6
Serguei Bezverkhi <sbezverk@cisco.com>
2023-07-06 22:35:54 +0200

    update workflow #6
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- build/set_kernel_params.sh
+++ build/set_kernel_params.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-sudo sysctl -w fs.inotify.max_user_instances=64000
+sudo sysctl -w net.bridge.bridge-nf-call-iptables=0
+sudo sysctl -w net.bridge.bridge-nf-call-ip6tables=0
+sudo sysctl -w fs.inotify.max_user_instances=65536
+sudo sysctl -w fs.inotify.max_user_watches=65536 
 sudo sysctl -w net.ipv4.udp_mem='1124736 10000000 67108864'
 sudo sysctl -w net.core.netdev_max_backlog=300000
 sudo sysctl -w net.core.optmem_max=67108864
