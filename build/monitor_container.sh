2fee468f0794378a719e090c96f16bc071ff084b
Serguei Bezverkhi <sbezverk@cisco.com>
2023-07-07 10:26:02 +0200

    add monitoring step
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- build/monitor_container.sh
+++ build/monitor_container.sh
@@ -38,4 +38,33 @@ else
         exit 1
 fi
 
+done=false
+found=false
+i=0
+
+while [ ${done} == false ]; do
+
+        msg=$(docker logs ${cid} | grep "client 10.1.1.3")
+        if [[ ${msg} != "" ]]; then
+                done=true
+                found=true
+        else
+                i=$((i+1))
+                if [[ ${i} -eq 12 ]]; then
+                        done=true
+                else
+                        sleep 10;
+                fi
+        fi
+done
+
+if [[ ${found} == true ]]; then
+        echo "bmp session with 10.1.1.3 came up"
+else
+    echo "container ${cn} failed to establish bmp session with 10.1.1.3, check gobmp and xr-1 containers' logs..."
+	docker logs ${cid}
+	docker logs $(docker ps | grep xr-1 | awk '{ print $1 }') 
+        exit 1
+fi 
+
 exit 0 
