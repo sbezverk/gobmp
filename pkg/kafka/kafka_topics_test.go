4df3cbf4c06906d94327e6278a8ccbe6f5dbf4ee
Serguei Bezverkhi <sbezverk@cisco.com>
2026-09-17 09:25:50 +0200

    bump up code coverage
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- pkg/kafka/kafka_topics_test.go
+++ pkg/kafka/kafka_topics_test.go
@@ -18,15 +18,36 @@ func (p *captureAsyncProducer) Input() chan<- *sarama.ProducerMessage {
 	return p.input
 }
 
-func TestPublishMessageMUPTopics(t *testing.T) {
+func TestPublishMessageTopics(t *testing.T) {
 	tests := []struct {
 		name    string
 		msgType int
 		topic   string
 	}{
+		{name: "peer", msgType: bmp.PeerStateChangeMsg, topic: PeerTopic},
+		{name: "unicast", msgType: bmp.UnicastPrefixMsg, topic: UnicastMessageTopic},
+		{name: "unicast IPv4", msgType: bmp.UnicastPrefixV4Msg, topic: UnicastMessageV4Topic},
+		{name: "unicast IPv6", msgType: bmp.UnicastPrefixV6Msg, topic: UnicastMessageV6Topic},
+		{name: "LS node", msgType: bmp.LSNodeMsg, topic: LSNodeMessageTopic},
+		{name: "LS link", msgType: bmp.LSLinkMsg, topic: LSLinkMessageTopic},
+		{name: "L3VPN", msgType: bmp.L3VPNMsg, topic: L3vpnMessageTopic},
+		{name: "L3VPN IPv4", msgType: bmp.L3VPNV4Msg, topic: L3vpnMessageV4Topic},
+		{name: "L3VPN IPv6", msgType: bmp.L3VPNV6Msg, topic: L3vpnMessageV6Topic},
+		{name: "LS prefix", msgType: bmp.LSPrefixMsg, topic: LSPrefixMessageTopic},
+		{name: "LS SRv6 SID", msgType: bmp.LSSRv6SIDMsg, topic: LSSRv6SIDMessageTopic},
+		{name: "EVPN", msgType: bmp.EVPNMsg, topic: EVPNMessageTopic},
+		{name: "SR policy", msgType: bmp.SRPolicyMsg, topic: SRPolicyMessageTopic},
+		{name: "SR policy IPv4", msgType: bmp.SRPolicyV4Msg, topic: SRPolicyMessageV4Topic},
+		{name: "SR policy IPv6", msgType: bmp.SRPolicyV6Msg, topic: SRPolicyMessageV6Topic},
+		{name: "FlowSpec", msgType: bmp.FlowspecMsg, topic: FlowspecMessageTopic},
+		{name: "FlowSpec IPv4", msgType: bmp.FlowspecV4Msg, topic: FlowspecMessageV4Topic},
+		{name: "FlowSpec IPv6", msgType: bmp.FlowspecV6Msg, topic: FlowspecMessageV6Topic},
+		{name: "VPLS", msgType: bmp.VPLSMsg, topic: VPLSMessageTopic},
 		{name: "combined", msgType: bmp.MUPMsg, topic: MUPMessageTopic},
 		{name: "IPv4", msgType: bmp.MUPV4Msg, topic: MUPMessageV4Topic},
 		{name: "IPv6", msgType: bmp.MUPV6Msg, topic: MUPMessageV6Topic},
+		{name: "statistics", msgType: bmp.StatsReportMsg, topic: StatsMessageTopic},
+		{name: "raw", msgType: bmp.BMPRawMsg, topic: RawMessageTopic},
 	}
 
 	for _, tt := range tests {
@@ -48,6 +69,17 @@ func TestPublishMessageMUPTopics(t *testing.T) {
 				t.Fatalf("PublishMessage() topic = %q, want %q", got, want)
 			}
 		})
+	}
+}
+
+func TestPublishMessageUnsupported(t *testing.T) {
+	producer := &captureAsyncProducer{
+		input: make(chan *sarama.ProducerMessage, 1),
+	}
+	p := &publisher{producer: producer}
+
+	if err := p.PublishMessage(9999, []byte("key"), []byte("message")); err == nil {
+		t.Fatal("PublishMessage() error = nil, want an error for an unsupported message type")
 	}
 }
 
